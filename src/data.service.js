e1da5ca2c540e53ca44d18c988b53cfe9b792e04
lucnsdev <lucns.dev@gmail.com>
2026-04-02 19:57:07 -0300

    Basic changes

--- src/data.service.js
+++ src/data.service.js
@@ -1,14 +1,13 @@
 import fs from "fs";
-
-const dir = "./raw";
+const raw = process.env.SECRETS_PATH;
 
 const dataService = {
 
   async getLastStatus() {
     if (!fs.existsSync(dir)) return undefined;
     try {
-      const data = await fs.readFileSync('./raw/data.json');
-      const jsonObject = JSON.parse(data);      
+      const data = await fs.readFileSync(`${raw}/data.json`);
+      const jsonObject = JSON.parse(data);
       return jsonObject["status"];
     } catch (err) {
       console.error('File read failure. Error:', err);
@@ -23,7 +22,7 @@ const dataService = {
         }
       });
     }
-    await fs.writeFile('./raw/data.json', jsonData, 'utf8', (err) => {
+    await fs.writeFile(`${raw}/data.json`, jsonData, 'utf8', (err) => {
       if (err) {
         console.error('An error occurred while writing file.', err);
       }
@@ -32,7 +31,7 @@ const dataService = {
   async getData() {
     if (!fs.existsSync(dir)) return { "status": "data_not_exists" };
     try {
-      const data = await fs.readFileSync('./raw/data.json');
+      const data = await fs.readFileSync(`${raw}/data.json`);
       const jsonObject = JSON.parse(data);
       return jsonObject;
     } catch (err) {
